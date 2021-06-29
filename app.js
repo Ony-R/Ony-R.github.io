d8e0b9c3169a61e1e9a61cbff00c6a23e6ae22a5
Ony.R <ony.ratsimbaharison@gmail.com>
2021-06-29 17:24:41 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const drums = new Howl({
+        "src": [
+          "./drums/drums.webm",
+          "./drums/drums.mp3"
+        ],
+        "sprite": {
+          "clap": [
+            0,
+            497.3922902494331
+          ],
+          "closed-hihat": [
+            2000,
+            318.4580498866212
+          ],
+          "crash": [
+            4000,
+            3908.095238095238
+          ],
+          "kick": [
+            9000,
+            652.4943310657604
+          ],
+          "open-hihat": [
+            11000,
+            1030.7256235827663
+          ],
+          "snare": [
+            14000,
+            577.5736961451248
+          ]
+        }
+});
+
+const drumkit = document.querySelector('.drumkit');
+function playDrum(event) {
+  if (event.target.classList.contains('pad')) {
+    event.preventDefault();
+    let soundToPlay = event.target.dataset.sound;
+    drums.play(soundToPlay);
+  }
+}
+
+function setViewportHeight() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+
+setViewportHeight();
+window.addEventListener('resize', () => {
+  setTimeout(setViewportHeight, 100);
+});
+
+drumkit.addEventListener('click', playDrum);
+drumkit.addEventListener('touchstart', playDrum);
